45a5f3471ba7da0dd1749318b6e04a4f4bda5a02
p-anja <pesic.anja@uns.ac.rs>
2021-06-23 22:48:24 +0200

    Homepage created

---
+++ src/main/resources/static/components/MainNavigation.vue
@@ -0,0 +1,182 @@
+<template>
+	<div id="navapp">
+		<nav id="navbar">
+			<div id="nav" :class="{sticky:active}">
+				<div class="nav-container" :class="toggleNavClass()">
+					<div class="navbar-item logo">
+						beelivery
+					</div>
+					<div id="search">
+						<!-- Search text field -->
+						<input placeholder="Search restaurants" type="text">
+					</div>
+					<div id="nav-links">
+						<slot></slot>
+						<router-link v-if="jws" to="/logout">Logout</router-link>
+						<router-link v-else to="/login">Login</router-link>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+<script>
+	module.exports =
+	{
+		data: function()
+		{
+			return{
+				search: '',
+				active: false,
+			}
+
+		},
+		methods:
+		{
+
+			toggleNavClass()
+			{
+				if(this.active == false)
+				{
+					return "navbar";
+				}
+					else
+				{
+					return "navbar-sticky";
+				}
+			},
+		},
+		computed:
+		{
+			jws: function() {
+				return localStorage.jws;
+			}
+		},
+        mounted()
+        {
+        },
+	}
+</script>
+<style scoped>
+	#navapp {
+		--h: 8vh;
+		background-color: #fff;
+	}
+
+	.navbar {
+		transition: 100ms;
+		padding: 0px 25px 0px 25px;
+	}
+
+	.navbar-sticky {
+		transition: 100ms;
+		padding: 0px 25px 0px 25px;
+	}
+
+	#nav {
+		transition: 150ms;
+		height: var(--h);
+		width: 100%;
+		top: 0;
+		z-index: 3;
+	}
+
+	#nav.sticky {
+		transition: 150ms;
+		background-color: #fff;
+		box-shadow: 0px 1px 10px #999;
+	}
+
+	.nav-container {
+		display: flex;
+		flex-direction: row;
+		/* flex-wrap: wrap; */
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.sticky > .nav-container {
+		flex-wrap: nowrap;
+	}
+
+	.nav-container > * {
+		min-height: var(--h);
+		flex-basis: 50%;
+	}
+
+
+	.navbar-dropdown:hover .navbar-list {
+		display: block;
+		top: 7vh;
+	}
+
+	#nav-links {
+		display: flex;
+		flex-direction: row;
+		align-content: center;
+		justify-content: flex-end;
+	}
+
+	#nav-links a {
+		display: grid;
+		place-items: center;
+		color: #311403;
+		text-decoration: none;
+		text-align: center;
+		font-size: 1.5rem;
+		font-weight: 400;
+		padding: 5px;
+	}
+
+	#nav-links a:hover {
+		color: var(--accent);
+	}
+
+	.navbar-item {
+		margin: 0;
+		padding: 0;
+	}
+
+	.logo {
+		flex-grow: 1;
+		display: flex;
+		align-items: center;
+	}
+
+	.navbar-sticky > .logo {
+		max-width: 13rem;
+	}
+
+	.nav-container {
+		max-width: 64vw;
+		margin: auto;
+	}
+
+	#nav > .nav-container > .logo {
+		font-family: "Poppins";
+		font-size: 1.5rem;
+		border: none;
+		color: #311403;
+		text-transform: uppercase;
+		font-weight: 600;
+	}
+
+	.navbar-list hr {
+		padding: 0px;
+		margin: 0px;
+	}
+
+	#nav.sticky > .nav-container > .logo{
+		font-size: 1.8rem;
+		border: none;
+		color: #e12454;
+		text-transform: uppercase;
+		font-weight: 600;
+	}
+
+	#search {
+		display: grid;
+		place-items: center;
+	}
+
+</style>
--- src/main/resources/static/views/Home.vue
+++ src/main/resources/static/views/Home.vue
@@ -1,6 +1,40 @@
 <template>
     <div id="home-main">
-        <h1>{{response}} from {{name}}</h1>
+        <main-navigation></main-navigation>
+        <div id="cover-main">
+            <div id="moto-container">
+                <div id="cover-moto">
+                    <h1 id="moto-title">Beelivery</h1>
+                    <p id="moto-text">Buzzing to keep you happy</p>
+                    <button class="button-primary">Sign up</button>
+                </div>
+            </div>
+            <img src="img/bonnie.png" alt="Logo">
+        </div>
+        <div id="card-home">
+            <div id="card-container">
+                <div class="card">
+                    <h2 class="card-header">Lorem</h2>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus asperiores maiores nam quia reprehenderit. Illum tempore quod dignissimos rerum laudantium dolorem iure vero est excepturi autem. Voluptatum architecto corporis fugit.</p>
+                </div>
+                <div class="card">
+                    <h2 class="card-header">Lorem</h2>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus asperiores maiores nam quia reprehenderit. Illum tempore quod dignissimos rerum laudantium dolorem iure vero est excepturi autem. Voluptatum architecto corporis fugit.</p>
+                </div>
+                <div class="card">
+                    <h2 class="card-header">Lorem</h2>
+                    <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus asperiores maiores nam quia reprehenderit. Illum tempore quod dignissimos rerum laudantium dolorem iure vero est excepturi autem. Voluptatum architecto corporis fugit.</p>
+                </div>
+            </div>
+        </div>
+        <div id="find-more">
+            <div id="find-more-container">
+                <h2>Find more restaurants of your liking</h2>
+                <button>
+                    Search
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -26,3 +60,88 @@ module.exports = {
     }
 };
 </script>
+
+<style scoped>
+    #home-main {
+        display: flex;
+        flex-direction: column;
+        background:var(--dark);
+        min-height: 100vh;
+    }
+
+    #cover-main {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        background-color: var(--primary);
+        height: 60vh;
+        padding: 0 10rem;
+    }
+
+    #cover-main img {
+        margin-left: auto;
+    }
+
+    #moto-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #cover-moto {
+        padding: 10px;
+    }
+
+    #card-home {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: #fafafa;
+    }
+
+    #card-container {
+        margin-top: -2rem;
+        --w: 1000px;
+        --n: 3;
+        width: 60vw;
+        display: grid;
+        background: none;
+        grid-template-columns: repeat(auto-fit, minmax(clamp(100% / (var(--n) + 1) + 0.1%, (var(--w) - 100vw) * 1000, 100%), 1fr));
+    }
+
+    .card {
+        display: flex;
+        flex-grow: 1;
+        flex-shrink: 1;
+        flex-basis: 0px;
+        flex-direction: column;
+        justify-content: space-around;
+        max-width: 20vw;
+        border-radius: 15px 15px 15px 15px;
+        box-shadow: 0px 0px 30px 0px rgba(0, 42, 106, 0.1);
+        padding: 2rem;
+        margin: 0rem 1rem;
+        background-color: #fff;
+        height: 400px;
+    }
+
+    #find-more {
+        display: grid;
+        place-items: center;
+        min-height: 40vh;
+    }
+
+    #find-more-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    #find-more-container h2 {
+        padding: 0 0 10px 0;
+    }
+
+    #find-more-container button {
+        align-self: center;
+    }
+
+</style>
