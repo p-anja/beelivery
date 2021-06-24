7c77dd57b4b5d36b386f4cfb80586df59cdcbbe9
p-anja <pesic.anja@uns.ac.rs>
2021-06-24 11:34:44 +0200

    Signin frontend added

---
+++ src/main/resources/static/views/Signin.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="signin-main">
+        <div id="signin-container">
+            <h2>Sign in to Beelivery</h2>
+            <p>Enter your account info</p>
+            <form id="signin-form" @submit="signin">
+                <b class="error">{{errors.signin}}</b>
+                <b class="error">{{errors.username}}</b>
+                <input type="text" v-model="username" placeholder="Username">
+                <b class="error">{{errors.password}}</b>
+                <input type="text" v-model="password" placeholder="Password">
+                <div class="spacer"></div>
+                <input class="button-primary" type="submit" value="Sign in">
+                <router-link to="/signup">Don't have an account? Signup now!</router-link>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {
+            username: '',
+            password: '',
+            errors: {
+                username: '',
+                password: '',
+                signin: '',
+            },
+        }
+    },
+    methods: {
+        validateInputs: function() {
+            this.errors.signin = '';
+            let usernameValid = !!this.username;
+            let passwordValid = !!this.password;
+            this.errors.username = usernameValid ? '' : 'Username required';
+            this.errors.password = passwordValid ? '' : 'Password required';
+            return usernameValid && passwordValid;
+        },
+
+        signin: function(e) {
+            e.preventDefault();
+            if(!this.validateInputs()) {
+                return;
+            }
+
+            let data = {
+                username: this.username,
+                password: this.password,
+            };
+            axios.post('signin', data)
+                .then(r => {
+                    localStorage.jws = r.data;
+                    this.$router.push('/');
+                })
+                .catch(() => this.errors.signin = 'Invalid username or password');
+        }
+    },
+};
+</script>
+
+<style scoped>
+#signin-main {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+    background: #fafafa;
+}
+
+#signin-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+    border: solid 1px #eee;
+    background: #fff;
+    border-radius: 10px;
+    text-align: center;
+}
+
+#signin-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-bottom: 0;
+    min-height: 25vh;
+}
+</style>
